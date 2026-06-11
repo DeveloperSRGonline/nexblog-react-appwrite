5e6c30a880c004d86ffcd2923d9b036a4e85c099
Shivam <shivamgarade05@gmail.com>
2026-06-11 21:51:19 +0530

    import and export variables production way

---
+++ src/variables/variables.js
@@ -0,0 +1,9 @@
+const variables = {
+  appwriteEndpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+  appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default variables;
