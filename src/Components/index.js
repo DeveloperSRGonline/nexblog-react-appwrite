974583510104ca59ba54313b3292a3832fd3b6f2
Shivam <shivamgarade05@gmail.com>
2026-06-15 23:09:40 +0530

    add PostForm and RTE components to index export

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,6 +5,23 @@ import Logo from "./Logo";
 import LogoutButton from "./Header/LogoutButton";
 import Input from "./Input";
 import Button from "./Button";
+import Select from "./Select";
+import Login from "./Login";
+import Signup from "./Signup";
+import RTE from "./RTE";
+import PostForm from "./PostForm.jsx";
 
-
-export {Header,Footer,Container,Logo,LogoutButton}
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutButton,
+  Select,
+  Button,
+  Input,
+  Login,
+  Signup,
+  RTE,
+  PostForm,
+};
