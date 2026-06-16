d13637ab7850a1f233ee5d445196e80ae64f67f1
Shivam <shivamgarade05@gmail.com>
2026-06-16 20:04:37 +0530

    add EditPost component for editing blog posts

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,29 @@
+import React,{useState,useEffect} from 'react'
+import {Container,PostForm} from "../Components"
+import appWriteService from "../Services/appwrite/config"
+import { useNavigate, useParams } from 'react-router-dom'
+
+const EditPost = () => {
+    const [post,setPost] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+      if(slug){
+        appWriteService.getPost(slug).then((post)=>{
+            if(post){
+                setPost(post)
+            }
+        })
+      }
+    })
+  return post ? (
+    <div className='py-8'>
+        <Container>
+            <PostForm post={post} />
+        </Container>
+    </div>
+  ) : null
+}
+
+export default EditPost
