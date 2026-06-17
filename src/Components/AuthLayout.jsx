bd03f3861231cbdc41be5c2b316f0ff2be41e798
Shivam <shivamgarade05@gmail.com>
2026-06-17 15:54:55 +0530

    fix: correct export syntax and update auth status selector in Protected component

--- src/Components/AuthLayout.jsx
+++ src/Components/AuthLayout.jsx
@@ -3,13 +3,21 @@ import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
 
-export default Protected = ({children,authentication = true}) => {
+const Protected = ({children,authentication = true}) => {
   const navigate = useNavigate()
-  const authStatus = useSelector((state) => state.auth.status)
+  const authStatus = useSelector((state) => state.authSlice.status)
 
   useEffect(() => {
     if(authentication && authStatus === false){
-        navigate
+        navigate("/login")
+    }else if(!authentication && authStatus === true){
+        navigate("/")
     }
   },[authStatus,navigate,authentication])
+
+  return (
+    <div>{children}</div>
+  )
 }
+
+export default Protected
