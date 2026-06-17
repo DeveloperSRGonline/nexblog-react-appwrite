68af9d62558ef4583f3a5e47656f8bf94e9d0ee2
Shivam <shivamgarade05@gmail.com>
2026-06-17 15:54:46 +0530

    refactor routing setup to use React Router and enhance component structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,77 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import AuthLayout from "./Components/AuthLayout.jsx"
+import App from './App.jsx'
+import Login from './Components/Login.jsx'
+import Signup from './Components/Signup.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import AddPosts from './pages/AddPost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+import AddPost from './pages/AddPost.jsx'
+
+const router = createBrowserRouter([
+  {
+    // root
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      }, {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        )
+      }, {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts />
+          </AuthLayout>
+        )
+      }, {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        )
+      }, {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout >
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        )
+      },{
+        path:"/post/:slug",
+        element:<Post />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>,
 )
