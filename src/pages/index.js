b8f8e9a1eb85711a64ed9505f107cb59f49d7e5d
Shivam <shivamgarade05@gmail.com>
2026-06-20 16:23:01 +0530

    refactor(pages): add index file for centralized component exports

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import AddPost from "./AddPost";
+import Login from "./Login";
+import Post from "./Post";
+import Signup from "./Signup";
+import EditPost from "./EditPost";
+import AllPosts from "./AllPosts";
+
+
+
+export { Home, AddPost, Login, Post, Signup, EditPost, AllPosts };
